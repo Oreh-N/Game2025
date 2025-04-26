c5dd372eb5fa2c08e28cf3b3ae55bdca62a7adb5
Anastasiia Orekhovych <nastia.orehowich@gmail.com>
2025-04-26 12:49:00 +0200

    Multiple units select

--- Assets/Scripts/UnitSelectionManager.cs
+++ Assets/Scripts/UnitSelectionManager.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class UnitSelectionManager : MonoBehaviour
 {
@@ -8,6 +10,12 @@ public class UnitSelectionManager : MonoBehaviour
 
 	public List<GameObject> unitsSelected = new List<GameObject>();
 	public List<GameObject> allUnits = new List<GameObject>();
+
+	private Camera cam;
+
+	public LayerMask clickable;
+	public LayerMask ground;
+	public GameObject groundMarker;
 
 	private void Awake()
 	{
@@ -17,9 +25,45 @@ public class UnitSelectionManager : MonoBehaviour
 		{ Instance = this; }
 	}
 
+	private void Start()
+	{
+		cam = Camera.main;
+	}
 
 
+	private void Update()
+	{
+
+		if (Input.GetMouseButtonDown(0))
+		{
+			RaycastHit hit;
+			Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+			// If we are hitting a clickble object
+			if (Physics.Raycast(ray, out hit, Mathf.Infinity, clickable))
+			{ SelectByClicking(hit.collider.gameObject); }
+			else  // Deselect all the units
+			{ DeselectAll(); }
+		}
+	}
+
+	private void SelectByClicking(GameObject unit)
+	{
+		DeselectAll();
+
+		unitsSelected.Add(unit);
+		EnableUnitMovement(unit, true);
+	}
+	private void DeselectAll()
+	{
+		
+	}
+
+	private void EnableUnitMovement(GameObject unit, bool canMove)
+	{
+		unit.GetComponent<UnitMovement>().enabled = canMove;
+
+
+	}
 
 }
-
-
