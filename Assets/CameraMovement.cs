9f249c1b737d5e967519a73b89fbd13b520e142f
Anastasiia Orekhovych <nastia.orehowich@gmail.com>
2025-07-07 00:39:11 +0200

    Add minimap camera

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    Camera main;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
