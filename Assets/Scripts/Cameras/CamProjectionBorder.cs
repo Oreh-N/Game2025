56e1a0dc1b750129c8cb0d1f0b855be12634a55d
Anastasiia Orekhovych <nastia.orehowich@gmail.com>
2025-08-24 18:57:56 +0200

    Try improve camera border

--- Assets/Scripts/Cameras/CamProjectionBorder.cs
+++ Assets/Scripts/Cameras/CamProjectionBorder.cs
@@ -24,23 +24,28 @@ public class CameraBorder : MonoBehaviour
         cam = Camera.main;
         line = GetComponentInChildren<LineRenderer>();
         display_ratio = Screen.width / Screen.height;
+        UpdateProjection();
     }
 
-    void Update()
-    {
-        cam_x_angle = cam.transform.rotation.x;
-        angle_with_ground = 90 - cam_x_angle;
-        ground_catheti = cam.transform.position.y * Mathf.Tan(cam_x_angle);
-        projection_width = cam.orthographicSize * display_ratio;
-        projection_length = cam.orthographicSize/ Mathf.Sin(angle_with_ground);
-        projection_height = cam.transform.position.y * 2;
-        line_pos[0].x = projection_width / 2;
-        line_pos[1] = new Vector3(projection_width / 2 , projection_height, projection_height);
-		line_pos[2] = new Vector3(-projection_width / 2, projection_height, projection_height);
-		line_pos[3].x = -projection_width / 2;
+	void UpdateProjection()
+	{
+		cam_x_angle = cam.transform.rotation.x;
+		angle_with_ground = 90 - cam_x_angle;
+		ground_catheti = cam.transform.position.y * Mathf.Tan(cam_x_angle);
+		projection_width = cam.orthographicSize * display_ratio;
+		projection_length = cam.orthographicSize / Mathf.Sin(angle_with_ground);
+		projection_height = cam.transform.position.y * 2;
+		line_pos[0].x = projection_height;
+		line_pos[1] = new Vector3(projection_height, projection_width, projection_width);
+		line_pos[2] = new Vector3(-projection_height, projection_width, projection_width);
+		line_pos[3].x = -projection_height;
 		line_pos[4].x = line_pos[0].x;
 
 		line.SetPositions(line_pos);
+	}
+
+	void Update()
+    {
     }
 }
 /*
