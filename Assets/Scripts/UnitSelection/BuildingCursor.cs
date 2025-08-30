be864541f1326aeae429385ff0ce9e7e0e9edc86
Anastasiia Orekhovych <nastia.orehowich@gmail.com>
2025-08-30 19:44:29 +0200

    Add simple building interface

---
+++ Assets/Scripts/UnitSelection/BuildingCursor.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+[RequireComponent(typeof(RawImage))]
+public class BuildingCursor : MonoBehaviour
+{
+
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
